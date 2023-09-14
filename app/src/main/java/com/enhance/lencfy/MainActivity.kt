d191a9aa7751f3e45148acdc36cf967707cb784c
Kshitiz-Mhto <kshititzmhto101@gmail.com>
2023-09-15 00:16:38 +0545

    feat: added splash support, updated app icon and added required resources

--- app/src/main/java/com/enhance/lencfy/MainActivity.kt
+++ app/src/main/java/com/enhance/lencfy/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Base_Theme_Lencfy)
         setContentView(R.layout.activity_main)
     }
 }
