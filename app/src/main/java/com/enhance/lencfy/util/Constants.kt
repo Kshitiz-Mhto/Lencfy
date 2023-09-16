cf5c3b8a6eb725613618058f65db49d4698197df
Kshitiz-Mhto <kshititzmhto101@gmail.com>
2023-09-16 12:01:25 +0545

    feat: added constants and external paths and additional meta data for authority for camera

---
+++ app/src/main/java/com/enhance/lencfy/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.enhance.lencfy.util
+
+object Constants {
+    const val LICENSE = ""
+    const val AUTHORITY = "com.enhance.lencfy.provider"
+    const val TEMP_IMAGEPATH= "/storage/emulated/0/Android/data/com.enhance.lencfy/files/Pictures/demo_directory/demo.jpg"
+
+}
